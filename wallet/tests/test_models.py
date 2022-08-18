9d1e10605a508b0382a0da39f1b1cb378de8de94
Uche Ogbu <Ucheogbu95@gmail.com>
2022-08-18 21:07:03 +0100

    modify tests

--- wallet/tests/test_models.py
+++ wallet/tests/test_models.py
@@ -1,4 +1,9 @@
 from django.test import TestCase
 
 class TestModels(TestCase):
-    ...
+    def setUp(self) -> None:
+        return super().setUp()
+        ...
+    def tearDown(self) -> None:
+        return super().tearDown()
+    
---
+++ wallet/tests/test_urls.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+class TestModels(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+        ...
+    def tearDown(self) -> None:
+        return super().tearDown()
+    
